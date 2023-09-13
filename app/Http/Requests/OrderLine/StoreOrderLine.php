112fb9ea1ef5101fc1aecdcfa4e1d829730897c0
Yvan <yvanquilliard@hotmail.fr>
2023-09-13 18:59:10 +0200

    Add function messages for StoreOrderLine.php and UpdateOrderLine.php

--- app/Http/Requests/OrderLine/StoreOrderLine.php
+++ app/Http/Requests/OrderLine/StoreOrderLine.php
@@ -24,11 +24,26 @@ class StoreOrderLine extends FormRequest
     public function rules()
     {
         return [
-
             'order_id' => 'required|integer',
             'product_id' => 'required|integer',
             'quantity' => 'required|integer|min:1',
             'unit_price' => 'required|numeric|min:0.01',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'order_id.required' => 'Veuillez indiquer l\'identifiant de la commande.',
+            'order_id.integer' => 'L\'identifiant de la commande doit être un nombre entier.',
+            'product_id.required' => 'Veuillez indiquer l\'identifiant du produit.',
+            'product_id.integer' => 'L\'identifiant du produit doit être un nombre entier.',
+            'quantity.required' => 'Veuillez indiquer la quantité.',
+            'quantity.integer' => 'La quantité doit être un nombre entier.',
+            'quantity.min' => 'La quantité doit être d\'au moins 1.',
+            'unit_price.required' => 'Veuillez indiquer le prix unitaire.',
+            'unit_price.numeric' => 'Le prix unitaire doit être un nombre.',
+            'unit_price.min' => 'Le prix unitaire doit être d\'au moins 0.01 €.',
         ];
     }
 }
--- app/Http/Requests/OrderLine/UpdateOrderLine.php
+++ app/Http/Requests/OrderLine/UpdateOrderLine.php
@@ -30,4 +30,20 @@ class UpdateOrderLine extends FormRequest
             'unit_price' => 'required|numeric|min:0.01',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'order_id.required' => 'Veuillez indiquer l\'identifiant de la commande.',
+            'order_id.integer' => 'L\'identifiant de la commande doit être un nombre entier.',
+            'product_id.required' => 'Veuillez indiquer l\'identifiant du produit.',
+            'product_id.integer' => 'L\'identifiant du produit doit être un nombre entier.',
+            'quantity.required' => 'Veuillez indiquer la quantité.',
+            'quantity.integer' => 'La quantité doit être un nombre entier.',
+            'quantity.min' => 'La quantité doit être d\'au moins 1.',
+            'unit_price.required' => 'Veuillez indiquer le prix unitaire.',
+            'unit_price.numeric' => 'Le prix unitaire doit être un nombre.',
+            'unit_price.min' => 'Le prix unitaire doit être d\'au moins 0.01 €.',
+        ];
+    }
 }
