d6ea87c4dfb8020d16a73995cd73752efac35633
Yvan <yvanquilliard@hotmail.fr>
2023-09-13 18:50:07 +0200

    Add function messages for StoreDeliveryAddress.php and UpdateDeliveryAddress.php

--- app/Http/Requests/DeliveryAddress/StoreDeliveryAddress.php
+++ app/Http/Requests/DeliveryAddress/StoreDeliveryAddress.php
@@ -37,10 +37,26 @@ class StoreDeliveryAddress extends FormRequest
     public function messages()
     {
         return [
-            'required' => 'Le champ :attribute est obligatoire.',
-            'integer' => 'Le champ :attribute doit être un nombre entier.',
-            'string' => 'Le champ :attribute doit être une chaîne de caractères.',
-            'max' => 'La longueur maximale du champ :attribute est de :max caractères.',
+            'order_id.required' => 'Veuillez sélectionner une commande.',
+            'order_id.integer' => 'Veuillez sélectionner une commande valide.',
+            'recipient_name.required' => 'Veuillez saisir le nom du destinataire.',
+            'recipient_name.string' => 'Le nom du destinataire doit être une chaîne de caractères.',
+            'recipient_name.max' => 'Le nom du destinataire ne doit pas dépasser 255 caractères.',
+            'recipient_phone.required' => 'Veuillez saisir le numéro de téléphone du destinataire.',
+            'recipient_phone.string' => 'Le numéro de téléphone du destinataire doit être une chaîne de caractères.',
+            'recipient_phone.max' => 'Le numéro de téléphone du destinataire ne doit pas dépasser 255 caractères.',
+            'address.required' => 'Veuillez saisir l\'adresse du destinataire.',
+            'address.string' => 'L\'adresse du destinataire doit être une chaîne de caractères.',
+            'address.max' => 'L\'adresse du destinataire ne doit pas dépasser 255 caractères.',
+            'postal_code.required' => 'Veuillez saisir le code postal du destinataire.',
+            'postal_code.string' => 'Le code postal du destinataire doit être une chaîne de caractères.',
+            'postal_code.max' => 'Le code postal du destinataire ne doit pas dépasser 255 caractères.',
+            'city.required' => 'Veuillez saisir la ville du destinataire.',
+            'city.string' => 'La ville du destinataire doit être une chaîne de caractères.',
+            'city.max' => 'La ville du destinataire ne doit pas dépasser 255 caractères.',
+            'country.required' => 'Veuillez saisir le pays du destinataire.',
+            'country.string' => 'Le pays du destinataire doit être une chaîne de caractères.',
+            'country.max' => 'Le pays du destinataire ne doit pas dépasser 255 caractères.',
         ];
     }
 
