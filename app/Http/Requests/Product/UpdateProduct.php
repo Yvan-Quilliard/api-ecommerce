d8af22fe395848bab50a7233e937e7c255b61588
Yvan <yvanquilliard@hotmail.fr>
2023-09-13 19:23:26 +0200

    Add function messages for StoreProduct.php and UpdateProduct.php

--- app/Http/Requests/Product/UpdateProduct.php
+++ app/Http/Requests/Product/UpdateProduct.php
@@ -32,4 +32,23 @@ class UpdateProduct extends FormRequest
             'category_id' => 'required|integer',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Veuillez saisir le nom du produit.',
+            'name.string' => 'Le nom du produit doit être une chaîne de caractères.',
+            'name.max' => 'Le nom du produit ne doit pas dépasser 255 caractères.',
+            'description.required' => 'Veuillez saisir la description du produit.',
+            'description.string' => 'La description du produit doit être une chaîne de caractères.',
+            'price.required' => 'Veuillez indiquer le prix du produit.',
+            'price.numeric' => 'Le prix du produit doit être un nombre.',
+            'price.min' => 'Le prix du produit doit être d\'au moins 0.01 €.',
+            'stock.required' => 'Veuillez indiquer le stock disponible du produit.',
+            'stock.integer' => 'Le stock du produit doit être un nombre entier.',
+            'category_id.required' => 'Veuillez sélectionner la catégorie du produit.',
+            'category_id.integer' => 'L\'identifiant de la catégorie du produit doit être un nombre entier.',
+        ];
+    }
+
 }
