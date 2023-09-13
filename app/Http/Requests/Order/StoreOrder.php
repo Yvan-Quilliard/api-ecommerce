6a41e2b1eebe933c944b12fb55dd1df0b2a95f7a
Yvan <yvanquilliard@hotmail.fr>
2023-09-13 18:54:32 +0200

    Add function messages for StoreOrder.php.php and UpdateOrder.php.php

--- app/Http/Requests/Order/StoreOrder.php
+++ app/Http/Requests/Order/StoreOrder.php
@@ -24,9 +24,13 @@ class StoreOrder extends FormRequest
     public function rules()
     {
         return [
-            'user_id' => 'required|integer',
-            'order_date' => 'required|date',
-            'status' => 'required|string|max:255',
+            'user_id.required' => 'Veuillez indiquer l\'identifiant de l\'utilisateur.',
+            'user_id.integer' => 'L\'identifiant de l\'utilisateur doit être un nombre entier.',
+            'order_date.required' => 'Veuillez indiquer la date de la commande.',
+            'order_date.date' => 'La date de la commande doit être une date valide.',
+            'status.required' => 'Veuillez saisir le statut de la commande.',
+            'status.string' => 'Le statut de la commande doit être une chaîne de caractères.',
+            'status.max' => 'Le statut de la commande ne doit pas dépasser 255 caractères.',
         ];
     }
 }
--- app/Http/Requests/Order/UpdateOrder.php
+++ app/Http/Requests/Order/UpdateOrder.php
@@ -29,4 +29,18 @@ class UpdateOrder extends FormRequest
             'status' => 'required|string|max:255',
         ];
     }
+
+
+    public function messages()
+    {
+        return [
+            'user_id.required' => 'Veuillez indiquer l\'identifiant de l\'utilisateur.',
+            'user_id.integer' => 'L\'identifiant de l\'utilisateur doit être un nombre entier.',
+            'order_date.required' => 'Veuillez indiquer la date de la commande.',
+            'order_date.date' => 'La date de la commande doit être une date valide.',
+            'status.required' => 'Veuillez saisir le statut de la commande.',
+            'status.string' => 'Le statut de la commande doit être une chaîne de caractères.',
+            'status.max' => 'Le statut de la commande ne doit pas dépasser 255 caractères.',
+        ];
+    }
 }
