5d9cfe14cd3c88b88c2d4289f898944eb5b533e2
Yvan <yvanquilliard@hotmail.fr>
2023-09-13 19:32:08 +0200

    Add function messages for StoreProductReview.php and UpdateProductReview.php

--- app/Http/Requests/ProductReview/StoreProductReview.php
+++ app/Http/Requests/ProductReview/StoreProductReview.php
@@ -31,4 +31,21 @@ class StoreProductReview extends FormRequest
             'rating' => 'required|integer|min:1|max:5',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'product_id.required' => 'Veuillez indiquer l\'identifiant du produit.',
+            'product_id.integer' => 'L\'identifiant du produit doit être un nombre entier.',
+            'user_id.required' => 'Veuillez indiquer l\'identifiant de l\'utilisateur.',
+            'user_id.integer' => 'L\'identifiant de l\'utilisateur doit être un nombre entier.',
+            'comment.required' => 'Veuillez saisir un commentaire.',
+            'comment.string' => 'Le commentaire doit être une chaîne de caractères.',
+            'rating.required' => 'Veuillez indiquer une note pour le produit.',
+            'rating.integer' => 'La note doit être un nombre entier.',
+            'rating.min' => 'La note doit être d\'au moins une étoile.',
+            'rating.max' => 'La note ne peut pas dépasser 5 étoile.',
+        ];
+    }
+
 }
